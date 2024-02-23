29190ac427dbcc8274c345e3b90d24ef87f8ff4b
Sergio Giraldo <sergio@giraldo.com.br>
2024-02-23 17:31:42 +0100

    feat!: day 22 & 23 completed ::by sergio giraldo @ 20240223T1731CET, gpg signed

--- __tests__/day23.ts
+++ __tests__/day23.ts
@@ -3,4 +3,11 @@
 	import Day23 from "../solutions/lib/day23";
 	
 	const helpers = require("../solutions/lib/helpers.ts");
-	
+	//no tests
+    test("SolveFirstStar", () => {
+        expect(1).toBe(1);
+    });
+    
+    test("SolveSecondStar", () => {
+        expect(2).toBe(2);
+    });
